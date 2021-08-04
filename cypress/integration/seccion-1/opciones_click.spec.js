c0c3929a2cea1b5145c592ec64158c4485742410
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-04 15:04:05 -0500

    Función Click Force-true

--- cypress/integration/seccion-1/opciones_click.spec.js
+++ cypress/integration/seccion-1/opciones_click.spec.js
@@ -16,6 +16,23 @@ describe("Opciones de Click", () => {
       .type("admin123");
     cy.get("#btnLogin").should("be.visible").click();
     cy.get("#menu_admin_viewAdminModule > b").should("be.visible").click();
-    cy.get("#menu_admin_nationality").should("be.visible").click();
+  });
+
+  it("Click Force true", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM");
+    cy.wait(1000);
+
+    cy.get("#txtUsername")
+      .should("be.visible")
+      .click()
+      .type("Admin")
+      .tab()
+      .should("be.visible")
+      .type("admin123");
+    cy.get("#btnLogin").should("be.visible").click();
+    cy.get("#menu_admin_viewAdminModule > b")
+      .should("be.visible")
+      .click({ force: true });
   });
 });
