ba0df14e8e1f473c493221358049edcfd9d06189
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-04 12:36:33 -0500

    Función Click Básico

---
+++ cypress/integration/seccion-1/opciones_click.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />;
+require("cypress-plugin-tab");
+
+describe("Opciones de Click", () => {
+  it("Click Sensillo", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM");
+    cy.wait(1000);
+
+    cy.get("#txtUsername")
+      .should("be.visible")
+      .click()
+      .type("Admin")
+      .tab()
+      .should("be.visible")
+      .type("admin123");
+    cy.get("#btnLogin").should("be.visible").click();
+    cy.get("#menu_admin_viewAdminModule > b").should("be.visible").click();
+    cy.get("#menu_admin_nationality").should("be.visible").click();
+  });
+});
