717753df1be5284000459f82d54f7a67b52c19ba
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-05 09:52:07 -0500

    Desarrollo de la solución del Reto

--- cypress/integration/seccion-1/primer_reto.spec.js
+++ cypress/integration/seccion-1/primer_reto.spec.js
@@ -1,13 +1,36 @@
 /// <reference types="Cypress" />;
+require("cypress-plugin-tab");
 
 describe("Primer Reto", () => {
   it("", () => {
     cy.visit("https://demoqa.com/webtables");
     cy.title().should("eq", "ToolsQA");
-    cy.wait(1500);
+    cy.wait(1000);
 
     cy.get("#searchBox").should("be.visible").type("Cierra");
-    cy.wait(1500);
+    cy.wait(1000);
+    cy.get("#searchBox").should("be.visible").clear();
+
+    //agregando campo
+    cy.get("#addNewRecordButton").should("be.visible").click();
+    cy.wait(1000);
+    cy.get("#firstName")
+      .should("be.visible")
+      .type("Germán")
+      .tab()
+      .type("García")
+      .tab()
+      .type("gmgarcia@gmail.com")
+      .tab()
+      .type("30")
+      .tab()
+      .type("30000")
+      .tab()
+      .type("Sistemas");
+    cy.get("#submit").should("be.visible").click();
+
+    cy.get("#searchBox").should("be.visible").type("Germán");
+    cy.wait(1000);
     cy.get("#searchBox").should("be.visible").clear();
   });
 });
