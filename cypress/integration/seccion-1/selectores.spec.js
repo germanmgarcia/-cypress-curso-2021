fad1cf0d7302ca526c8b30d92ccc9fbed4c35322
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-05 10:19:56 -0500

    Selector por Atributos

--- cypress/integration/seccion-1/selectores.spec.js
+++ cypress/integration/seccion-1/selectores.spec.js
@@ -1,13 +1,23 @@
 /// <reference types="Cypress" />;
 
 describe("Tipos de Selectores", () => {
-  it("Selector por id", () => {
+  it("Selector por Id", () => {
+    cy.visit("https://demoqa.com/text-box");
+    cy.title().should("eq", "ToolsQA");
+    cy.wait(1000);
+
+    //selector por Id
+    cy.get("#userName").should("be.visible").type("Germán");
+    cy.get("#userEmail").should("be.visible").type("demo@gmail.com");
+  });
+
+  it.only("Selector por Atributo", () => {
     cy.visit("https://demoqa.com/text-box");
     cy.title().should("eq", "ToolsQA");
     cy.wait(1000);
 
     //selector por id
-    cy.get("#userName").should("be.visible").type("Germán");
-    cy.get("#userEmail").should("be.visible").type("demo@gmail.com");
+    cy.get("[placeholder='Full Name']").should("be.visible").type("Juan Peréz");
+    // cy.get("#userEmail").should("be.visible").type("demo@gmail.com");
   });
 });
