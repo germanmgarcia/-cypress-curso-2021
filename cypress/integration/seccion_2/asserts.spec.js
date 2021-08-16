74549853885f4a3cde659ccfa0b0d71ca4697774
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-15 22:20:55 -0500

    Obtener el texto

--- cypress/integration/seccion_2/asserts.spec.js
+++ cypress/integration/seccion_2/asserts.spec.js
@@ -16,4 +16,19 @@ describe("Asserts", () => {
 
     cy.get(".product-container").find(".product-image-container").eq(0).click();
   });
+
+  it.only("Assert find, eq, validando que el vestido es nuevo", () => {
+    cy.visit("http://automationpractice.com/index.php");
+    cy.title().should("eq", "My Store");
+    cy.wait(1000);
+
+    cy.get(".product-container").find(".product-image-container").eq(2).click();
+
+    cy.get("#product_condition .editable").then((e) => {
+      let estado = e.text();
+      if (estado === "New") {
+        cy.log("El vestido es Nuevo");
+      }
+    });
+  });
 });
