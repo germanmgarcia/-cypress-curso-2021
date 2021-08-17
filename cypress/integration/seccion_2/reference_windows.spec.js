0ddb193109f6fd846a4f81f84ca068bc8fdd8c41
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-17 16:43:48 -0500

    Referencia Windows p2
    URL

--- cypress/integration/seccion_2/reference_windows.spec.js
+++ cypress/integration/seccion_2/reference_windows.spec.js
@@ -8,4 +8,13 @@ describe("Referencias a Windows", () => {
 
     cy.document().should("have.property", "charset").and("eq", "UTF-8");
   });
+
+  it("Windows URL", () => {
+    cy.visit("https://testsheepnz.github.io/random-number.html");
+    cy.title().should("eq", "The Number Game");
+    cy.wait(1500);
+
+    cy.url().should("include", "random-number.html");
+    cy.url().should("eq", "https://testsheepnz.github.io/random-number.html");
+  });
 });
