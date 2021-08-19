d2c1b42f9a53568b120451c9b56c5f2b750af66b
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-19 10:25:02 -0500

    Drag and Drop 2

--- cypress/integration/seccion_3/mouse.spec.js
+++ cypress/integration/seccion_3/mouse.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />;
 require("cypress-file-upload");
-require('@4tw/cypress-drag-drop')
+require("@4tw/cypress-drag-drop");
 
 describe("Cypress eventos Mouse", () => {
   it("Drag and Drop", () => {
@@ -9,6 +9,18 @@ describe("Cypress eventos Mouse", () => {
     cy.title().should("eq", "The Internet");
     cy.wait(tiempo);
 
-    cy.get("#column-a").drag("#column-b", { force: true})
+    cy.get("#column-a").drag("#column-b", { force: true });
+  });
+
+  it.only("Drag and Drop 2", () => {
+    const tiempo = 1000;
+    cy.visit("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+    cy.title().should("eq", "Selenium Easy Demo - Drag and Drop Demo");
+    cy.wait(tiempo);
+
+    cy.get("#todrag > :nth-child(2)").drag("#mydropzone", { force: true });
+    cy.get("#todrag > :nth-child(3)").drag("#mydropzone", { force: true });
+    cy.get("#todrag > :nth-child(4)").drag("#mydropzone", { force: true });
+    cy.get("#todrag > :nth-child(5)").drag("#mydropzone", { force: true });
   });
 });
