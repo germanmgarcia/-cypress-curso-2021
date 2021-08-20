70a4136ec23ab0969080062ec7c34215b26504c8
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-20 10:42:34 -0500

    Table Filter

--- cypress/integration/seccion_3/tablas.spec.js
+++ cypress/integration/seccion_3/tablas.spec.js
@@ -21,7 +21,7 @@ describe("Elementos de una tabla", () => {
     cy.wait(tiempo);
   });
 
-  it.only("Seleccionar por medio de EQ", () => {
+  it("Seleccionar por medio de EQ", () => {
     const tiempo = 1500;
     cy.visit(
       "https://www.seleniumeasy.com/test/table-records-filter-demo.html"
@@ -38,5 +38,24 @@ describe("Elementos de una tabla", () => {
       .eq(4)
       .should("contain", "All")
       .click({ force: true });
+  });
+
+  it.only("Filter", () => {
+    const tiempo = 1500;
+    cy.visit(
+      "https://www.seleniumeasy.com/test/table-records-filter-demo.html"
+    );
+    cy.title().should("eq", "Selenium Easy - Table Data Filter Demo");
+    cy.wait(tiempo);
+
+    cy.get("[type='button']").filter(".btn-warning");
+    cy.get("[type='button']").filter(".btn-success");
+    cy.wait(tiempo);
+    cy.get("[type='button']").filter(".btn-warning").click();
+    cy.wait(tiempo);
+    cy.get("[type='button']")
+      .filter(".btn-success")
+      .should("contain", "Green")
+      .click();
   });
 });
