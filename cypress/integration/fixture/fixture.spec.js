f1be26922dd16298f75444a90fbdcd5c0105441e
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-22 16:08:57 -0500

    Alias

--- cypress/integration/fixture/fixture.spec.js
+++ cypress/integration/fixture/fixture.spec.js
@@ -1,19 +1,25 @@
 /// <reference types="Cypress" />;
 
 describe("Carga por fixture", () => {
+  //   before(() => {
+  //     cy.fixture("datos").then((data) => {
+  //       globalThis.data = data;
+  //     });
+  //   });
+
   before(() => {
-    cy.fixture("datos").then((data) => {
-      globalThis.data = data;
-    });
+    cy.fixture("datos").as("datos_json"); //Alias
   });
   it("Test Cargando desde JSON", () => {
-    cy.visit("https://demoqa.com/text-box");
-    cy.title().should("eq", "ToolsQA");
-    cy.wait(1500);
-    cy.get("#userName").should("be.visible").type(data.nombre);
-    cy.get("#userEmail").should("be.visible").type(data.email);
-    cy.get("#currentAddress").should("be.visible").type(data.direccion1);
-    cy.get("#permanentAddress").should("be.visible").type(data.direccion2);
-    cy.get("#submit").should("be.visible").click();
+    cy.get("@datos_json").then((data) => {
+      cy.visit("https://demoqa.com/text-box");
+      cy.title().should("eq", "ToolsQA");
+      cy.wait(1500);
+      cy.get("#userName").should("be.visible").type(data.nombre);
+      cy.get("#userEmail").should("be.visible").type(data.email);
+      cy.get("#currentAddress").should("be.visible").type(data.direccion1);
+      cy.get("#permanentAddress").should("be.visible").type(data.direccion2);
+      cy.get("#submit").should("be.visible").click();
+    });
   });
 });
