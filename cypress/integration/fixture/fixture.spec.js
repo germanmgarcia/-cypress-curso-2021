6b0e8563fd69441ad27328db31cce954f931a84c
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-22 16:28:08 -0500

    Fixture solución del Reto

--- cypress/integration/fixture/fixture.spec.js
+++ cypress/integration/fixture/fixture.spec.js
@@ -12,14 +12,16 @@ describe("Carga por fixture", () => {
   });
   it("Test Cargando desde JSON", () => {
     cy.get("@datos_json").then((data) => {
-      cy.visit("https://demoqa.com/text-box");
-      cy.title().should("eq", "ToolsQA");
-      cy.wait(1500);
-      cy.get("#userName").should("be.visible").type(data.nombre);
-      cy.get("#userEmail").should("be.visible").type(data.email);
-      cy.get("#currentAddress").should("be.visible").type(data.direccion1);
-      cy.get("#permanentAddress").should("be.visible").type(data.direccion2);
-      cy.get("#submit").should("be.visible").click();
+      data.forEach((element) => {
+        cy.visit("https://demoqa.com/text-box");
+        cy.title().should("eq", "ToolsQA");
+        cy.wait(1500);
+        cy.get("#userName").should("be.visible").type(element.nombre);
+        cy.get("#userEmail").should("be.visible").type(element.email);
+        cy.get("#currentAddress").should("be.visible").type(element.direccion1);
+        cy.get("#permanentAddress").should("be.visible").type(element.direccion2);
+        cy.get("#submit").should("be.visible").click();
+      });
     });
   });
 });
