12a6970fce46a99e7ccf9c1c8ba757a525cbcb9c
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-22 12:06:36 -0500

    Reload

--- cypress/integration/seccion_4/navegacion.spec.js
+++ cypress/integration/seccion_4/navegacion.spec.js
@@ -28,4 +28,28 @@ describe("Navegación entre páginas", () => {
     cy.go("forward");
     cy.go("forward");
   });
+
+  it.only("Reload", () => {
+    const tiempo = 1000;
+    cy.visit("https://demoqa.com/");
+    cy.title().should("eq", "ToolsQA");
+    cy.wait(tiempo);
+
+    cy.get(":nth-child(1) > :nth-child(1) > .avatar")
+      .should("be.visible")
+      .click({ force: true });
+    cy.wait(tiempo);
+
+    cy.get(":nth-child(1) > .element-list > .menu-list > #item-0")
+      .should("be.visible")
+      .click({ force: true });
+    cy.wait(tiempo);
+
+    cy.get("#userName").should("be.visible").type("Germán");
+    cy.get("#userEmail").should("be.visible").type("gmgarcia@gmail.com");
+
+    cy.reload();
+    cy.wait(tiempo);
+    cy.go("back");
+  });
 });
