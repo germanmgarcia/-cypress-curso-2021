043dfd25d3ca790bff4a87cef4e3c840982f40ef
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-22 17:48:45 -0500

    Api Consulta desde Cypress comparando los resultados (Expect)

--- cypress/integration/Api/api.spec.js
+++ cypress/integration/Api/api.spec.js
@@ -7,8 +7,24 @@ describe("Api consultas", () => {
     datos.its("status").should("equal", 200);
   });
 
-  it("Test Api", () => {
+  it("Test Api Error", () => {
     datos = cy.request("https://rickandmortyapi.com/api/episode2");
     datos.its("status").should("equal", 200);
+  });
+
+  it.only("Test Api comparado consulta", () => {
+    cy.request({
+      method: "GET",
+      url: "https://rickandmortyapi.com/api/episode",
+      headers: {
+        accept: "application/json",
+      },
+    }).then((response) => {
+      let datos;
+      datos = JSON.parse(JSON.stringify(response.body));
+      cy.log(datos);
+
+      expect(datos.results[0]).has.property("name", "Pilot");
+    });
   });
 });
