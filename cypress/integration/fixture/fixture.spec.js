58574808e895ea1314ef4cbc0d2b2ccf2fd0da6a
Germán <gmgarcia@ucundinamarca.edu.co>
2021-08-22 16:06:37 -0500

    Fixture Cargando el Archivo Json

---
+++ cypress/integration/fixture/fixture.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />;
+
+describe("Carga por fixture", () => {
+  before(() => {
+    cy.fixture("datos").then((data) => {
+      globalThis.data = data;
+    });
+  });
+  it("Test Cargando desde JSON", () => {
+    cy.visit("https://demoqa.com/text-box");
+    cy.title().should("eq", "ToolsQA");
+    cy.wait(1500);
+    cy.get("#userName").should("be.visible").type(data.nombre);
+    cy.get("#userEmail").should("be.visible").type(data.email);
+    cy.get("#currentAddress").should("be.visible").type(data.direccion1);
+    cy.get("#permanentAddress").should("be.visible").type(data.direccion2);
+    cy.get("#submit").should("be.visible").click();
+  });
+});
